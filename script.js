bfdac3ad25ca8fc2bd803fbbc01859249b130d2a
ThangNP03 <2003nguyenphucthang@gmail.com>
2022-05-04 10:50:34 +0700

    Updates Homepage
    Updates Homepage Thang-NP03

---
+++ script.js
@@ -0,0 +1,77 @@
+
+$('.image-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   
+
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+    ],
+    
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+
+    
+    
+  });
+
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    dots:true,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+    ],
+    
+  });
